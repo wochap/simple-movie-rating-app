05b21bdbd39c63f7472c3da08e228d9d30d35284
wochap <gean.marroquin@gmail.com>
2017-05-27 18:34:21 -0500

    update not-found-screen

--- src/app/screens/NotFound/components/NotFoundScreen.vue
+++ src/app/screens/NotFound/components/NotFoundScreen.vue
@@ -1,6 +1,19 @@
 <template>
-  <div>
-    <h1>NotFoundScreen</h1>
+  <div class="c-not-found-screen">
+    <div class="o-hero">
+      <div class="o-hero__body">
+        <h1 class="c-not-found-screen__title">
+          <span>404</span> Page not found
+        </h1>
+
+        <router-link
+          :to="{name: 'AppHome'}"
+          class="c-link"
+        >
+          Go home
+        </router-link>
+      </div>
+    </div>
   </div>
 </template>
 
