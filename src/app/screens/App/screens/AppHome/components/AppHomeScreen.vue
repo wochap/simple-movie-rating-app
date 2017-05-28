60a6506534f85e4122dd1dafae5e9247732ec5f1
wochap <gean.marroquin@gmail.com>
2017-05-27 21:42:37 -0500

    feat: display movies

--- src/app/screens/App/screens/AppHome/components/AppHomeScreen.vue
+++ src/app/screens/App/screens/AppHome/components/AppHomeScreen.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    <h1>AppHomeScreen</h1>
+  <div class="o-wrapper u-py4">
+    <h2 class="u-hide">Movies</h2>
+    <ul class="o-grid o-grid--gutter-3 u-list-reset o-grid--stretch">
+      <li
+        class="o-grid__item u-12/12 u-6/12@mobile u-4/12@tablet u-2/12@laptop"
+      >
+        <movie-card-placeholder />
+      </li>
+      <li
+        v-for="movie in movies"
+        :key="movie.id"
+        class="o-grid__item u-12/12 u-6/12@mobile u-4/12@tablet u-2/12@laptop"
+      >
+        <movie-card v-bind="movie" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-  export default {}
+  import MovieCard from './MovieCard'
+  import MovieCardPlaceholder from './MovieCardPlaceholder'
+  import {mapGetters} from 'vuex'
+
+  export default {
+    components: {
+      MovieCard,
+      MovieCardPlaceholder
+    },
+    computed: {
+      ...mapGetters({
+        movies: 'getMovies'
+      })
+    }
+  }
 </script>
 
 <style lang="scss">
---
+++ src/app/screens/App/screens/AppHome/components/MovieCard.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="c-movie-card">
+    <router-link
+      :to="{name: 'MoviesView', params: {id, slug}}"
+      class="c-movie-card__image-link"
+    >
+      <img :src="imageUrl" :alt="name" />
+    </router-link>
+
+    <h3 class="c-movie-card__name">
+      <router-link
+        :to="{name: 'MoviesView', params: {id, slug}}"
+      >
+        {{name}}
+      </router-link>
+    </h3>
+    <p class="c-movie-card__rating">
+      Rating: <span>{{rating | formatRating}}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      id: String,
+      imageUrl: String,
+      name: String,
+      slug: String,
+      rating: Number
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
