0e580ad5f6caa07f53fe82012cefb9a45e1be17c
wochap <gean.marroquin@gmail.com>
2017-05-28 03:24:09 -0500

    feat: add FormControlText, FormField

---
+++ src/app/common/components/FormControlText.vue
@@ -0,0 +1,42 @@
+<template>
+  <form-field
+    :label="label"
+    :id="id"
+  >
+    <div class="c-form-control c-form-control--text">
+      <input
+        class="c-form-control__input"
+        type="text"
+        :placeholder="placeholder"
+        :id="id"
+        :value="value"
+        @input="handleInput"
+      />
+    </div>
+  </form-field>
+</template>
+
+<script>
+  import rndm from 'rndm'
+
+  export default {
+    props: {
+      value: [String, Number],
+      label: String,
+      placeholder: String
+    },
+    data () {
+      return {
+        id: rndm(8)
+      }
+    },
+    methods: {
+      handleInput (event) {
+        this.$emit('input', event.target.value)
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
---
+++ src/app/common/components/FormField.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="['c-form-field', {'c-form-field--horizontal': horizontal}]">
+    <label class="c-form-field__label" :for="id">{{label}}</label>
+    <div class="c-form-field__control">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      horizontal: {
+        type: Boolean,
+        default: false
+      },
+      label: String,
+      id: String
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
