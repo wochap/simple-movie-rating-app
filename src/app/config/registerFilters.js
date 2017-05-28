bcc24a60524c7113ea70523a02fd27b9755ecf58
wochap <gean.marroquin@gmail.com>
2017-05-28 04:56:56 -0500

    feat: add formatDate

--- src/app/config/registerFilters.js
+++ src/app/config/registerFilters.js
@@ -12,4 +12,22 @@ export default function registerFilters (Vue: any) {
 
     return accounting.formatMoney(value, '$ ', 2, '.', ',')
   })
+
+  Vue.filter('formatDate', (value: Date) => {
+    if (!value) return '-'
+
+    const date = new Date(value)
+    const monthNames = [
+      'January', 'February', 'March',
+      'April', 'May', 'June', 'July',
+      'August', 'September', 'October',
+      'November', 'December'
+    ]
+
+    const day = date.getDate() + 1
+    const monthIndex = date.getMonth()
+    const year = date.getFullYear()
+
+    return `${day} ${monthNames[monthIndex]} ${year}`
+  })
 }
