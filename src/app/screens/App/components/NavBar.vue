4908c6517501be9bde7b7dfb2b96b5abc8de427d
wochap <gean.marroquin@gmail.com>
2017-05-27 20:42:41 -0500

    feat: add NavBar

---
+++ src/app/screens/App/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="c-nav-bar">
+    <div class="o-wrapper">
+      <h1 class="c-nav-bar__title">
+        <router-link :to="{name: 'AppHome'}">MovieApp</router-link>
+      </h1>
+      <search-box />
+    </div>
+  </header>
+</template>
+
+<script>
+  import SearchBox from './SearchBox'
+
+  export default {
+    components: {
+      SearchBox
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
