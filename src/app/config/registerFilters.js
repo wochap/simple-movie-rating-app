b1d71d1f84432bc267794401d6592dcd4e3ff11f
wochap <gean.marroquin@gmail.com>
2017-05-27 23:34:01 -0500

    feat: add currency filter

--- src/app/config/registerFilters.js
+++ src/app/config/registerFilters.js
@@ -1,7 +1,15 @@
 // @flow
 
+import accounting from 'accounting'
+
 export default function registerFilters (Vue: any) {
   Vue.filter('formatRating', (value: number): string => {
     return value.toFixed(1)
+  })
+
+  Vue.filter('currency', (value: number) => {
+    if (!value) return '-'
+
+    return accounting.formatMoney(value, '$ ', 2, '.', ',')
   })
 }
