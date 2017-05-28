37c2a4d98ead01dc4174949933033b9fb01cd8e7
wochap <gean.marroquin@gmail.com>
2017-05-28 05:24:19 -0500

    import actors-view-screen

--- src/app/config/registerComponents.js
+++ src/app/config/registerComponents.js
@@ -1,5 +1,13 @@
-// import components from common
+import ResultLabel from 'app/common/components/ResultLabel'
+import VueDialog from 'app/common/components/VueDialog'
+import FormField from 'app/common/components/FormField'
+import FormControlText from 'app/common/components/FormControlText'
+import MovieCard from 'app/common/components/MovieCard'
 
 export default function registerComponents (Vue) {
-  // Vue.component('custom-component', CustomComponent)
+  Vue.component('result-label', ResultLabel)
+  Vue.component('vue-dialog', VueDialog)
+  Vue.component('form-field', FormField)
+  Vue.component('form-control-text', FormControlText)
+  Vue.component('movie-card', MovieCard)
 }
